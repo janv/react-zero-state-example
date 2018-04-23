5c8bc76a353b65f54db352fe8459cf2edd2efd32
Jan Varwig <jvarwig@small-improvements.com>
2018-04-23 22:52:14 +0200

    index.js Typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import App from './routing/App';
 
 ReactDOM.render(<App />, document.getElementById('app'));
